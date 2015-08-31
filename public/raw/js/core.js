4937d8387ae325dece5d8a0edbc00f354cdc74c6
Spenser C <bananatrongo@gmail.com>
2015-08-31 02:50:10 +0000

    init auth buildout

--- public/raw/js/core.js
+++ public/raw/js/core.js
@@ -1,6 +1,3 @@
-var myFirebaseRef = new Firebase("https://reep.firebaseio.com/");
-
-console.log(myFirebaseRef);
 
 var taskModel = {
     summary: "",
@@ -9,6 +6,22 @@ var taskModel = {
     focus: [],
     complete: false
 }
+
+
+
+//Show/hide new form
+$('.task-new__close').on('click', function(){
+  $( "#task-new" ).slideUp( "fast", function() {
+    $( "#task-new" ).hide();
+  });
+});
+
+$('#add-task').on('click', function(){
+  $( "#task-new" ).slideDown( "fast", function(){
+    $( "#task-new" ).show();
+  })
+});
+
 
 //Task header toggle !!Review
 $('.task__header').on('click', function(){
@@ -55,6 +68,8 @@ $('#task_submit').on('click', function(){
 
 
 
+
+
 //HELPERS
 var validateModel = function(model){ //Maybe put in notifications here?
   if (model.summary == "" || model.summary == undefined) return false;
@@ -70,3 +85,29 @@ var toggleSelected = function(self){
     $(self).attr('data-selected', 'true'); 
   }
 }
+
+
+// Notifications
+function Notification(text, type, trigger) {
+  this.text = text;
+  this.type = type || "";
+  
+  this.init = function(){
+    var note = $("<div class='note " + this.type + "' >" + this.text + "<span class='notification__close ion-close-circled'></span></div>");
+    $('.notifications').append(note);
+    if (trigger){
+      $(note).on('click', function(){ trigger()  })
+    }
+  }
+  
+  this.init();
+}
+
+$('.notifications').on('click', function(){
+  if ($(event.target).attr('class').indexOf('note') != -1){
+    $( event.target ).slideUp( 200, function() {
+      $( event.target ).remove();
+    })
+  }
+});
+
---
+++ public/raw/js/login.js
@@ -0,0 +1,74 @@
+var ref = new Firebase("https://reep.firebaseio.com/");
+
+//Login button
+$('#user-login').on('click', function(){
+
+  ref.authWithPassword({
+    email    : $('#login-email-input').val().trim(),
+    password : $('#login-pass-input').val().trim()
+  }, function(error, authData) {
+    if (error) {
+      if (error.toString().indexOf('user does not exist') != -1){
+        
+        var removeYellow = function(){
+          $('#user-register').addClass('bg-darkblue');
+          $('#user-register').removeClass('bg-yellow');
+        }
+        $('#user-register').addClass('bg-yellow');
+        $('#user-register').removeClass('bg-darkblue');
+        
+        new Notification( 
+          "That user doesn't exist, but you can <span class='bg-yellow'>register</span> below with that information",'alert', removeYellow 
+        );
+        
+      } else {
+        if (authData === undefined){
+          new Notification("That's not valid user info", 'alert');
+        } else {
+          new Notification(authData);
+        }
+       
+      }
+      
+    } else {
+      new Notification("Login succesful!");
+    }
+  });
+});
+
+
+//Register button
+$('#user-register').on('click', function(){
+  ref.createUser({
+    email    : $('#login-email-input').val().trim(),
+    password : $('#login-pass-input').val().trim()
+  }, function(error, userData) {
+    if (error) {
+      new Notification(error, 'alert' );
+    } else {
+      console.log(userData, userData.uid);
+      new Notification( "Hoorah - You've created an account!" );
+    }
+  });
+});
+
+//Reset email option
+$('#user-resetpass').on('click', function(){
+  ref.resetPassword({
+      email : $('#login-email-input').val().trim(),
+    }, function(error) {
+    if (error === null) {
+      new Notification("Password reset email sent successfully");
+    } else {
+      if (error.toString().indexOf('Route') != -1) {
+        new Notification('Please enter your email below so we can get you reset', 'alert')
+      } else {
+        new Notification(error, 'alert');
+      }
+    }
+  });
+});
+
+
+
+
